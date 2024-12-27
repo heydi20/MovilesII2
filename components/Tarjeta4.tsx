49a22cffb9e604f8cb2d527465028801974aaf25
heydi20 <heydipantoja1@gmail.com>
2024-12-27 10:26:38 -0500

    cambios clase

--- components/Tarjeta4.tsx
+++ components/Tarjeta4.tsx
@@ -1,49 +1,59 @@
-import { Button, Image, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React, { useState } from 'react'
+import { Button, Image, Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import React, { useState } from 'react';
 
-export default function Tarjeta4(props:any) {
-  const [visible, setvisible] = useState(false)
-    return (
-      <TouchableOpacity onPress={() => setvisible(true)} style={styles.card}>
-        <Text>{props.datos.results.gender.name.title}</Text>
-        <Modal
-          visible={visible}
-          transparent={true}
-        >
-          <View style={styles.modal}>
-            <Image
-              source={{ uri: props.datos.results.picture.large}}
-              style={styles.imag}
-            />
-          </View>
-        </Modal>
-      </TouchableOpacity>
-    );
-  }
-  
-  const styles = StyleSheet.create({
-    card: {
-      marginBottom: 10,
-      padding: 10,
-      backgroundColor: 'lightgray',
-    },
-    imageContainer: {
-      alignItems: 'center',
-      marginVertical: 10,
-    },
-    imag: {
-      height: 250,
-      width: 400,
-      resizeMode: 'contain',
-    } , 
-    modal: {
-      backgroundColor: 'pink',  
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',  
-    },
+export default function Tarjeta4(props: any) {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <TouchableOpacity onPress={() => setVisible(true)} style={styles.card}>
+      <Text>{props.datos.name.first} {props.datos.name.last}</Text>
+      
+      <Modal
+        visible={visible}
+        transparent={true}
+      >
+        <View style={styles.modal}>
+          <Image
+            source={{ uri: props.datos.picture.large }} 
+            style={styles.imag}
+          />
+          <Text style={styles.txtTitulo}>{props.datos.name.first} {props.datos.name.last}</Text>
+          <Text style={styles.txtTitulo2}>Nombre localización:{props.datos.location.street.name}</Text>
+          <Text style={styles.txtTitulo3}>Descripción:{props.datos.location.timezone.description}</Text>
+          <Text style={styles.txtTitulo3}>Correo:{props.datos.email}</Text>
+          <Button color='purple'title="Cerrar" onPress={() => setVisible(false)} />
+        </View>
+      </Modal>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    marginBottom: 10,
+    padding: 10,
+    backgroundColor: 'lightgray',
+  },
+  imag: {
+    height: 250,
+    width: 250,  
+    resizeMode: 'contain',
+  },
+  modal: {
+    backgroundColor: '#b29ee9',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  txtTitulo: {
+    fontSize: 50,
+  },
+  txtTitulo2: {
+    fontSize: 20,
+  },
+  txtTitulo3: {
+    fontSize: 20,
+    justifyContent: 'center',
     
-    txtTitulo: {
-      fontSize: 50
-    }
-  });
+  },
+});
--- screens/Pagina8Screen.tsx
+++ screens/Pagina8Screen.tsx
@@ -1,6 +1,5 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { FlatList } from 'react-native-gesture-handler';
+import { StyleSheet, Text, View, FlatList, ImageBackground } from 'react-native';
+import React, { useEffect, useState } from 'react';
 import Tarjeta4 from '../components/Tarjeta4';
 
 export default function Pagina8Screen() {
@@ -8,23 +7,46 @@ export default function Pagina8Screen() {
 
     useEffect(() => {
         async function leer() {
+
             const resp = await fetch('https://randomuser.me/api/?results=20');
             const json = await resp.json();
-            setDatos(json);
+            setDatos(json.results);
+
         }
         leer();
     }, []);
+
     return (
-        <View>
-            <Text>PERSONAJES</Text>
-            <FlatList
-                data={datos}
-                renderItem={({ item }) => (
-                    <Tarjeta4 datos={item} />
-                )}
-            />
-        </View>
-    )
+        <ImageBackground
+            source={{ uri: "https://4kwallpapers.com/images/walls/thumbs_3t/19.jpg" }}
+            style={styles.img}
+
+        >
+            <View style={styles.container}>
+                <Text style={styles.title}>USUARIOS</Text>
+                <FlatList
+                    data={datos}
+                    renderItem={({ item }) => (
+                        <Tarjeta4 datos={item} />
+                    )}
+                />
+            </View>
+        </ImageBackground>
+    );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 20,
+    },
+    img: {
+        flex: 1,
+    },
+});
